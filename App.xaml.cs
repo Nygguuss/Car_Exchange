deaaf242934a93f070e422af6e3fc44588501f65
Nygguuss <100948681+Nygguuss@users.noreply.github.com>
2024-04-12 19:51:48 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Car_Exchange
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
