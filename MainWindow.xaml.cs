19ac491308ef6d063e6a7788bcff8ac373266b5e
Nygguuss <100948681+Nygguuss@users.noreply.github.com>
2024-04-13 19:38:31 +0200

    podstawowe info samochodu dodano,

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,28 @@ namespace Car_Exchange
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Model> carList { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            carInit();
+        }
+
+        public void carInit()
+        {
+            carList = new List<Model>();
+
+            Model car1 = new Model("Opel", "Corsa", 2016, "Szary", 220, "src\\auto.png");
+            Model car2 = new Model("Dacia", "Sandero", 2012, "Niebieski", 220, "src\\auto2.png");
+            Model car3 = new Model("Ford", "Mk3", 2017, "Czerwony", 280, "src\\auto3.png");
+            Model car4 = new Model("Scania", "R730", 2015, "Brązowy", 180, "src\\auto4.png");
+
+            carList.Add(car1);
+            carList.Add(car2);
+            carList.Add(car3);
+            carList.Add(car4);
+            this.DataContext = carList;
         }
     }
 }
---
+++ pages/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace Car_Exchange
+{
+    public class Model
+    {
+        public string Marka { get; set; }
+        public string ModelSamochodu { get; set; }
+        public int RokProdukcji { get; set; }
+        public string Kolor { get; set; }
+        public double PredkoscMaksymalna { get; set; }
+        public string SciezkaDoZdjecia { get; set; }
+
+        public Model(string marka, string modelS, int rokProdukcji, string kolor, double predkosc, string sciezkaDoZdjecia)
+        {
+            Marka = marka;
+            ModelSamochodu = modelS;
+            RokProdukcji = rokProdukcji;
+            Kolor = kolor;
+            PredkoscMaksymalna = predkosc;
+            SciezkaDoZdjecia = sciezkaDoZdjecia;
+        }
+
+        public string Informacje()
+        {
+            return $"Marka: {Marka}, Model: {ModelSamochodu}, Rok produkcji: {RokProdukcji}, Kolor: {Kolor}, Prędkość: {PredkoscMaksymalna} km/h";
+        }
+    }
+}
