3c3fdb9609d115c2c505ed5567ee84dd535687c4
Nygguuss <100948681+Nygguuss@users.noreply.github.com>
2024-04-18 11:44:22 +0200

    dodano cofanie do strony głównej i podstawową klase użytkownika

---
+++ classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Exchange.classes
+{
+    class User
+    {
+        public string NazwaUzytkownika { get; set; }
+        public string Haslo { get; set; }
+        public string Email { get; set; }
+
+        public User(string na, string h, string e)
+        {
+            NazwaUzytkownika = na;
+            Haslo = h;
+            Email = e;
+        }
+
+
+    }
+}
